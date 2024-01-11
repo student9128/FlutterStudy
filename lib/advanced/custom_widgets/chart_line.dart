c1aa709c249ac21912f18a63022b0c4e7a1d1fd7
student9128 <jingpengchn@163.com>
2024-01-11 17:52:22 +0800

    add:折线图连接处实现平滑过渡

--- lib/advanced/custom_widgets/chart_line.dart
+++ lib/advanced/custom_widgets/chart_line.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -126,43 +127,83 @@ class ChartLine extends CustomPainter {
       } else {
         points3.add(Offset(startX + unitX * i, p0Y + 4 * unitY - (i - 12) * unitY));
       }
-      if(i==0||i==4){
-        points4.add(Offset(startX + unitX * i, p0Y - i * unitY));
-      }else if(i==8){
-
-        points4.add(Offset(startX + unitX * i, p0Y - 4 * unitY + (i - 4) * unitY));
-      }
-      else if(i==12){
-        points4.add(Offset(startX + unitX * i, p0Y - 4 * unitY + (i - 4) * unitY));
-      }else if(i==16){
-        points4.add(Offset(startX + unitX * i, p0Y - 4 * unitY + (i - 4) * unitY));
-      }
     }
     _basePaint..color=Colors.brown..strokeWidth=7;
     canvas.drawPoints(PointMode.points, points3, _basePaint);
     _basePaint..color=Colors.orange;
     canvas.drawPoints(PointMode.points, points4, _linePaint);
     final path3 = Path();
-    path3.moveTo(points4[0].dx, points4[0].dy);
+    Path path4 = Path();
+    Path path5 = Path();
+    // path3.moveTo(points4[0].dx, points4[0].dy);
+    path3.moveTo(points3[0].dx, points3[0].dy);
+    path4.moveTo(points3[0].dx, points3[0].dy);
 
-    for (int i = 1; i < points4.length - 2; i++) {
-      final x0 = points4[i].dx;
-      final y0 = points4[i].dy;
-      final x1 = points4[i + 1].dx;
-      final y1 = points4[i + 1].dy;
-
-      final cx0 = x0 + (x1 - points4[i - 1].dx) / 6;
-      final cy0 = y0 + (y1 - points4[i - 1].dy) / 6;
-      final cx1 = x1 - (points4[i + 2].dx - x0) / 6;
-      final cy1 = y1 - (points4[i + 2].dy - y0) / 6;
-
-      if(i%4==0){
-
-      path3.cubicTo(cx0, cy0, cx1, cy1, x1, y1);
-      }
+    Offset a = points3[0];
+    Offset b = points3[0];
+    Offset c = points3[1];
+    var factor=0.1;
+    for (int i = 1; i < points3.length; i++) {
+      double x1 = (c.dx - a.dx) * factor + b.dx;
+      double y1 = (c.dy - a.dy) * factor + b.dy;
+      a = b;
+      b = c;
+      c = points3[min(points3.length - 1, i + 1)];
+      double x2 = (a.dx - c.dx) * factor + b.dx;
+      double y2 = (a.dy - c.dy) * factor + b.dy;
+      path3.cubicTo(x1, y1, x2, y2, b.dx, b.dy);
     }
-    _linePaint..color=Colors.yellow;
+    path4=generatePath(points3, 0.1, path4);
+    // points4.add(Offset(unitX,unitY*8));
+    path5.moveTo(unitX*2,unitY*2);
+    points4.add(Offset(unitX*2,unitY*2));
+    points4.add(Offset(unitX*4,unitY*8));
+    // points4.add(Offset(unitX*4.5,unitY*6));
+    points4.add(Offset(unitX*10,unitY*2));
+    points4.add(Offset(unitX*16,unitY*8));
+    path5=generatePath(points4, 0.35, path5);
+
+    // for (int i = 1; i < points4.length - 2; i++) {
+    //   final x0 = points4[i].dx;
+    //   final y0 = points4[i].dy;
+    //   final x1 = points4[i + 1].dx;
+    //   final y1 = points4[i + 1].dy;
+    //
+    //   final cx0 = x0 + (x1 - points4[i - 1].dx) / 6;
+    //   final cy0 = y0 + (y1 - points4[i - 1].dy) / 6;
+    //   final cx1 = x1 - (points4[i + 2].dx - x0) / 6;
+    //   final cy1 = y1 - (points4[i + 2].dy - y0) / 6;
+    //
+    //   if(i%4==0){
+    //
+    //   path3.cubicTo(cx0, cy0, cx1, cy1, x1, y1);
+    //   }
+    // }
+    _linePaint..color=Colors.blue..strokeWidth=10..strokeCap=StrokeCap.round..strokeJoin=StrokeJoin.round;
     canvas.drawPath(path3, _linePaint);
+    _linePaint..color=Colors.cyanAccent..strokeWidth=2..strokeCap=StrokeCap.round..strokeJoin=StrokeJoin.round;
+    canvas.drawPath(path4, _linePaint);
+    _linePaint..color=Colors.lightGreen..strokeWidth=2..strokeCap=StrokeCap.round..strokeJoin=StrokeJoin.round;
+    canvas.drawPath(path5, _linePaint);
+  }
+
+  Path generatePath(List<Offset> points3,double factor, Path path4) {
+    var temp=Offset.zero;
+     for(int i = 1; i < points3.length; i++){
+     var current = Offset(points3[i].dx,points3[i].dy);
+     var pre = Offset(points3[i-1].dx,points3[i-1].dy);
+     var next = Offset(points3[i+1<points3.length?i+1:i].dx,points3[i+1<points3.length?i+1:i].dy);
+     var control1 =pre+temp;
+    temp =((next-pre)/2)*factor;
+      debugPrint('next-pre=${next-pre}');
+    if((next-pre).dx<=20){
+      debugPrint('next-pre=${next-pre}走吗了');
+      temp = Offset(0, temp.dy);
+    }
+     var control2 =current-temp;
+     path4.cubicTo(control1.dx, control1.dy, control2.dx, control2.dy, current.dx, current.dy);
+    }
+     return path4;
   }
 
   _drawAxis(Canvas canvas) {
@@ -173,7 +214,7 @@ class ChartLine extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+  bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
   }
 }
